9251c132a52cd159723ed3085c0036aed5b45db3
Temitope Edwards <r0kq6x13@students.codeinstitute.net>
2024-10-04 17:59:32 +0100

    first commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+let mon = document.getElementById('monday');
+let tue = document.getElementById('tuesday');
+let wed = document.getElementById('wednesday');
+let thur = document.getElementById('thursday');
+let fri = document.getElementById('friday');
+let overlay = document.getElementById('overlay');
+
+
+function showOverlay() {
+    
+}
