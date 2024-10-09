dc1367c7145bf068bb88764c2200909faa924e19
Temitope Edwards <r0kq6x13@students.codeinstitute.net>
2024-10-09 09:55:45 +0100

    added to events and day details files. Linked popup to pages with javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,8 +4,22 @@ let wed = document.getElementById('wednesday');
 let thur = document.getElementById('thursday');
 let fri = document.getElementById('friday');
 let overlay = document.getElementById('overlay');
-
+let popup = document.getElementById('popup');
 
 function showOverlay() {
-    
+    overlay.style.display = 'flex';
+}
+
+function hideOverlay () {
+    if (overlay.style.display === 'flex'){
+       overlay.style.display = 'none';
+    } 
+}
+
+function eventDetails () {
+    window.location.href='events-details.html';
+}
+
+function dayDetails () {
+    window.location.href = 'day-details.html';
 }
